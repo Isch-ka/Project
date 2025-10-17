da5c34e2c16959d8382054933f2302f24b29dceb
Isch-ka <igorkozlowcasper@gmail.com>
2025-10-17 14:34:11 +0300

    Update data.js

--- data.js
+++ data.js
@@ -2,17 +2,18 @@ const dataProducts = `
 [
     {
        "name": "Офис лешего",
-       "url": "картинка1.jpg",
+       "url": "images/картинка1.jpg",
        "price": "$550"
     },
     {
        "name": "Блок питания",
-       "url": "картинка2.jpg",
+       "url": "images/картинка2.jpg",
        "price": "$52"
     },
       {
        "name": "Российские IT-технологии",
-       "url": "картинка3.jpg",
+       "url": "images/картинка3.jpg",
        "price": "$200"
     }
+
 ]`
